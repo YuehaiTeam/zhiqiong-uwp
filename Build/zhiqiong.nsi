0b011818b99d2424b3d4a076e8c1398fb7b7e980
xiaoyu <7547189+xytoki@users.noreply.github.com>
2022-04-16 20:29:20 +0800

    🎉 init

---
+++ Build/zhiqiong.nsi
@@ -0,0 +1,70 @@
+﻿Name "志琼·原神地图"
+OutFile "..\Dist\zhiqiong_installer_1.0.2.exe"
+Unicode True
+InstallDir "$TEMP"
+RequestExecutionLevel admin
+ManifestDPIAware true
+ShowInstDetails show
+BrandingText " "
+SetCompressor zlib
+AutoCloseWindow true
+Icon "map.ico"
+InstallColors 0 4008636142
+LoadLanguageFile "${NSISDIR}\Contrib\Language files\SimpChinese.nlf"
+XPStyle on
+!include LogicLib.nsh
+!include WinMessages.nsh
+Page instfiles "" LogFont
+Function LogFont
+    Push $R0
+    Push $R1
+    FindWindow $R0 "#32770" "" $HWNDPARENT
+    CreateFont $R1 "Microsoft Yahei" "8" "400"
+    GetDlgItem $R0 $R0 1016
+    SendMessage $R0 ${WM_SETFONT} $R1 0
+    FindWindow $R0 "#32770" "" $HWNDPARENT
+    CreateFont $R1 "Microsoft Yahei" "8" "400"
+    GetDlgItem $R0 $R0 1006
+    SendMessage $R0 ${WM_SETFONT} $R1 0
+    Pop $R1
+    Pop $R0
+FunctionEnd
+Function installWebView2
+	# If this key exists and is not empty then webview2 is already installed
+	ReadRegStr $0 HKLM \
+        	"SOFTWARE\WOW6432Node\Microsoft\EdgeUpdate\Clients\{F3017226-FE2A-4295-8BDF-00C3A9A7E4C5}" "pv"
+	${If} ${Errors} 
+	${OrIf} $0 == ""
+		DetailPrint "Webview2未安装。"
+		File "MicrosoftEdgeWebview2Setup.exe"
+		nsExec::ExecToLog '"$INSTDIR\MicrosoftEdgeWebview2Setup.exe" /install'
+		SetDetailsPrint both
+    ${Else}
+		DetailPrint "Webview2已安装。"
+	${EndIf}
+FunctionEnd
+Section "Dummy Section" SecDummy
+    MessageBox MB_OKCANCEL|MB_ICONEXCLAMATION "本次安装需要在您的系统中信任该软件的签名根证书，请确认您信任该软件的发布者！" IDOK OK IDCANCEL CANCEL
+    OK:
+        SetOutPath "$INSTDIR"
+        DetailPrint "> 检查运行环境..."
+        Call installWebView2
+        DetailPrint "> 信任签名证书..."
+        File /oname=@yuehaiteam_zhiqiong.cer "zhiqiong.cer"
+        nsExec::ExecToLog "powershell.exe -Command certutil -addstore 'Root' '$INSTDIR\@yuehaiteam_zhiqiong.cer'"
+        Delete "$INSTDIR\@yuehaiteam_zhiqiong.cer"
+        DetailPrint "> 安装程序组件..."
+        File /oname=@yuehaiteam_zhiqiong.msix "..\AppPackages\zhiqiong_1.0.3.0_x64_Test\zhiqiong_1.0.3.0_x64.msix"
+        nsExec::ExecToLog "powershell.exe -Command Add-AppxPackage -Path '$INSTDIR\@yuehaiteam_zhiqiong.msix' -DeferRegistrationWhenPackagesAreInUse"
+        Delete "$INSTDIR\@yuehaiteam_zhiqiong.msix"
+        DetailPrint "> 放行本地连接..."
+        nsExec::ExecToLog "CheckNetIsolation LoopbackExempt -a -n=zhiqiong_tbtgzvrf5srwm"
+        DetailPrint "> 激活应用模块..."
+        nsExec::ExecToLog "powershell.exe -Command start ms-gamebar:activate/zhiqiong_tbtgzvrf5srwm_App_zhiqiong"
+        DetailPrint "> 即将完成..."
+        MessageBox MB_OK|MB_ICONINFORMATION "安装完成！请按Win+G打开Xbox Game Bar使用悬浮地图。"
+        Goto END
+    CANCEL:
+        Quit
+    END:
+SectionEnd
---
+++ MainPage.xaml.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Linq;
+using Windows.Foundation;
+using Windows.UI.Xaml.Controls;
+using System.Collections.Generic;
+using Microsoft.Web.WebView2.Core;
+using Windows.UI.Popups;
+using Windows.UI.Xaml.Navigation;
+
+namespace zhiqiong
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public bool isInGameBar = false;
+        public MainPage()
+        {
+
+            InitializeComponent();
+            WvInit();
+        }
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            var param = e.Parameter;
+            if (param.ToString() == "GAMEBAR")
+            {
+                isInGameBar = true;
+            }
+        }
+        /// <summary>
+        /// Run JS in Webview2 on init
+        /// </summary>
+        public async void WvInit()
+        {
+            await webView.EnsureCoreWebView2Async();
+            webView.CoreWebView2.Navigate("https://webstatic.mihoyo.com/app/ys-map-cn/index.html#/map/2");
+            await webView.CoreWebView2.AddScriptToExecuteOnDocumentCreatedAsync(@"!function(){const s = document.createElement('script')
+                s.src = 'https://zhiqiong.vercel.app/sharedmap.user.js?t='+Math.floor(new Date().getTime()/(1000*3600*24))*(3600*24)
+                s.onerror = () => { alert('共享地图加载失败，请检查是否可以连接到 https://zhiqiong.vercel.app '); }
+                window.addEventListener('DOMContentLoaded',()=>{document.head.appendChild(s);window.addEventListener('contextmenu', (e)=>{e.stopImmediatePropagation()},true);})}()
+                document.addEventListener('focus',(e)=>{if(e.target.tagName==='INPUT'||e.target.tagName==='TEXTAREA')window.chrome.webview.postMessage('INPUT')}, true);
+            ");
+            webView.CoreWebView2.NewWindowRequested += CoreWebView2_NewWindowRequested;
+            webView.CoreWebView2.ContextMenuRequested += CoreWebView2_ContextMenuRequested;
+            webView.CoreWebView2.NavigationStarting += CoreWebView2_NavigationStarting;
+            webView.CoreWebView2.WebMessageReceived += CoreWebView2_WebMessageReceived;
+        }
+        /// <summary>
+        /// Handle message
+        /// </summary>
+        public void CoreWebView2_WebMessageReceived(object sender, CoreWebView2WebMessageReceivedEventArgs e)
+        {
+            string j = e.WebMessageAsJson;
+            var jValue = Windows.Data.Json.JsonValue.Parse(j).GetString();
+            if (jValue == "INPUT" && isInGameBar)
+            {
+                InputBox();
+            }
+        }
+        /// <summary>
+        /// Change UserAgent
+        /// </summary>
+        public void CoreWebView2_NavigationStarting(object sender, CoreWebView2NavigationStartingEventArgs e)
+        {
+            var settings = webView.CoreWebView2.Settings;
+            // don't change if modified
+            if (settings.UserAgent.Contains("zhiqiong-uwp/"))
+            {
+                return;
+            }
+            settings.UserAgent = settings.UserAgent + " zhiqiong-uwp/" + (isInGameBar ? "gamebar" : "webview");
+        }
+        /// <summary>
+        /// Open default browser instead
+        /// </summary>
+        public async void CoreWebView2_NewWindowRequested(object sender, CoreWebView2NewWindowRequestedEventArgs e)
+        {
+            // preventdefault
+            e.Handled = true;
+            if (this.isInGameBar)
+            {
+                // Opening window is not allowed in gamebar, so open prompt box
+                ContentDialog dialog = new ContentDialog();
+                TextBox inputTextBox = new TextBox();
+                dialog.Content = inputTextBox;
+                inputTextBox.Text = e.Uri;
+                dialog.Title = "暂不支持在悬浮窗中打开外部链接，请复制到浏览器打开";
+                dialog.PrimaryButtonText = "复制并关闭";
+                dialog.SecondaryButtonText = "取消";
+                if (await dialog.ShowAsync() == ContentDialogResult.Primary)
+                {
+                    // copy to clipboard
+                    Windows.ApplicationModel.DataTransfer.DataPackage dataPackage = new Windows.ApplicationModel.DataTransfer.DataPackage();
+                    dataPackage.SetText(inputTextBox.Text);
+                    Windows.ApplicationModel.DataTransfer.Clipboard.SetContent(dataPackage);
+                }
+            }
+            else
+            {
+                var uri = new Uri(e.Uri);
+                await Windows.System.Launcher.LaunchUriAsync(uri);
+            }
+        }
+        /// <summary>
+        /// Change contextmenu
+        /// </summary>
+        public void CoreWebView2_ContextMenuRequested(object sender, CoreWebView2ContextMenuRequestedEventArgs args)
+        {
+            IList<CoreWebView2ContextMenuItem> menuList = args.MenuItems;
+            // remove
+            bool hasSelectAll = false;
+            bool hasReload = false;
+            string[] ignoredList = { "other", "saveImageAs", "copyImage", "copyImageLocation", "createQrCode", "saveAs", "print", "back", "forward" };
+            for (int index = 0; index < menuList.Count; index++)
+            {
+                if (ignoredList.Contains(menuList[index].Name))
+                {
+                    menuList.RemoveAt(index);
+                    index--;
+                }
+                else if (menuList[index].Name == "selectAll")
+                {
+                    hasSelectAll = true;
+                }
+                else if (menuList[index].Name == "reload")
+                {
+                    hasReload = true;
+                }
+            }
+            if (!hasReload && !hasSelectAll)
+            {
+                // no ctxmenu on non-input elements
+                args.Handled = true;
+            }
+            // add
+            if (hasSelectAll && isInGameBar)
+            {
+                CoreWebView2ContextMenuItem subItem = webView.CoreWebView2.Environment.CreateContextMenuItem("无法输入请点我", null, CoreWebView2ContextMenuItemKind.Command);
+                subItem.CustomItemSelected += delegate (CoreWebView2ContextMenuItem send, Object ex)
+                {
+                    this.InputBox();
+                };
+                menuList.Insert(0, subItem);
+            }
+        }
+        public async void InputBox()
+        {
+            // Get value of current input
+            var jCurrentValue = await webView.CoreWebView2.ExecuteScriptAsync(@"(document.activeElement.tagName==='INPUT'||document.activeElement.tagName==='TEXTAREA')?document.activeElement.value:''");
+            var currentValue = Windows.Data.Json.JsonValue.Parse(jCurrentValue).GetString();
+            // Get type of current input
+            var jCurrentType = await webView.CoreWebView2.ExecuteScriptAsync(@"(document.activeElement.tagName==='INPUT')?document.activeElement.type:'other'");
+            var currentType = Windows.Data.Json.JsonValue.Parse(jCurrentType).GetString();
+            // Prompt using ContentDialog
+            ContentDialog dialog = new ContentDialog();
+            if (currentType == "password")
+            {
+                PasswordBox inputTextBox = new PasswordBox();
+                dialog.Content = inputTextBox;
+                inputTextBox.Password = currentValue;
+            }
+            else
+            {
+                TextBox inputTextBox = new TextBox();
+                dialog.Content = inputTextBox;
+                inputTextBox.Text = currentValue;
+            }
+            dialog.Title = "请按Win+G打开Xbox Game Bar后，在此输入" + (currentType == "password" ? "密码" : "内容");
+            dialog.IsSecondaryButtonEnabled = true;
+            dialog.PrimaryButtonText = "输入";
+            dialog.SecondaryButtonText = "取消";
+            IAsyncOperation<ContentDialogResult> tsk = dialog.ShowAsync();
+            if (await tsk == ContentDialogResult.Primary)
+            {
+                // escape single quote
+                string escapedInput = "";
+                if (currentType == "password")
+                {
+                    PasswordBox inputTextBox = (PasswordBox)dialog.Content;
+                    escapedInput = inputTextBox.Password.Replace("'", "\\'");
+                }
+                else
+                {
+                    TextBox inputTextBox = (TextBox)dialog.Content;
+                    escapedInput = inputTextBox.Text.Replace("'", "\\'");
+                }
+                await webView.CoreWebView2.ExecuteScriptAsync(@"if(document.activeElement.tagName==='INPUT'||document.activeElement.tagName==='TEXTAREA'){
+                            document.activeElement.value = '" + escapedInput + @"';
+                            document.activeElement.dispatchEvent(new Event('input'));
+                            document.activeElement.blur();
+                        }");
+            }
+            else
+            {
+                // cancel
+                await webView.CoreWebView2.ExecuteScriptAsync(@"if(document.activeElement.tagName==='INPUT'||document.activeElement.tagName==='TEXTAREA'){
+                            document.activeElement.blur();
+                        }");
+            }
+        }
+    }
+
+}
